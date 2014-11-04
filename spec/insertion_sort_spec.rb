4ce61f38b23fa0c194bd4c308eb5c40397191124
Andrew Sinner <andrewsinner@gmail.com>
2014-11-03 20:52:02 -0800

    Appease the cop

--- spec/insertion_sort_spec.rb
+++ spec/insertion_sort_spec.rb
@@ -8,17 +8,17 @@ describe Array do
     array = (1..10_000).to_a.shuffle
     assert_equal((1..10_000).to_a, array.sorts(:insertion))
   end
-  
+
   it 'should be benchmarked' do
     puts 'Insertion sort -----------------'
     array = (1..10_000).to_a
     puts 'Best case'
     puts Benchmark.measure { array.sorts(:insertion) }
-  
+
     array = (1..10_000).to_a.reverse
     puts 'Worst case'
     puts Benchmark.measure { array.sorts(:insertion) }
-  
+
     array = (1..10_000).to_a.shuffle
     puts 'Random case'
     puts Benchmark.measure { array.sorts(:insertion) }
