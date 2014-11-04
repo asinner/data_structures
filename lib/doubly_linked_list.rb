96d6c2717c098712f04df91321a8f6f94ff005c9
Andrew Sinner <andrewsinner@gmail.com>
2014-11-03 19:46:23 -0800

    Add doubly linked list, spec

--- lib/doubly_linked_list.rb
+++ lib/doubly_linked_list.rb
@@ -1,14 +1,71 @@
 class List
-  attr_accessor :head
+  attr_accessor :head, :size
 
   class Node < Struct.new(:value)
     attr_accessor :next, :previous
+  end
+
+  def initialize
+    self.size = 0
   end
 
   def add(node)
     c = head
     node.next = c
     c.previous = node if c
+    self.size += 1
     self.head = node
+  end
+
+  def search(value)
+    results = []
+    c = head
+    while c
+      results.push(c) if c.value == value
+      c = c.next
+    end
+    results
+  end
+
+  def delete(node)
+    node.previous.next = node.next if node.previous
+    node.next.previous = node.previous if node.next
+    if node.object_id == head.object_id
+      self.head = node.next
+    end
+    self.size -= 0
+    node
+  end
+
+  def deduplicate
+    values = {}
+    c = head
+    while c
+      if values[c.value]
+        delete(c)
+      end
+      values[c.value] = c
+      c = c.next
+    end
+  end
+
+  def deduplicate_vanilla
+    c = head
+    while c
+      j = c.next
+      while j
+        delete(j) if j.value == c.value
+        j = j.next
+      end
+      c = c.next
+    end
+  end
+
+  def to_s
+    c = head
+    while c
+      puts c.value
+      c = c.next
+    end
   end
 end
--- spec/doubly_linked_list_spec.rb
+++ spec/doubly_linked_list_spec.rb
@@ -5,22 +5,44 @@ require 'benchmark'
 
 describe List do
 
-  let(:list) do
-    list = List.new
-    200.times do
-      list.add(List::Node.new(rand(1..100)))
+  before do
+    @list = List.new
+    2.times do
+      (1..100).each { |i| @list.add(List::Node.new(i)) }
     end
-    list
   end
 
   it 'should add a node' do
-    n = List::Node.new('Andrew')
-    list.add(n)
-    assert list.head, n
+    n = List::Node.new(101)
+    @list.add(n)
+    assert @list.head, n
+    assert @list.head.next.value = 100
   end
 
-  it 'should delete all nodes with a given value' do
-    n = List::Node.new('Bob')
-    list.delete('Bob')
+  it 'should find a node with a given value' do
+    n1 = List::Node.new(150)
+    n2 = List::Node.new('Doesnt get added')
+    @list.add(n1)
+    assert_equal @list.search(150), [n1]
+    assert_equal @list.search('Doesnt get added'), []
+  end
+
+  it 'should delete any given node' do
+    n = List::Node.new(125)
+    @list.add(n)
+    assert_equal @list.delete(n), n
+    assert_equal @list.search(125), []
+  end
+
+  it 'should deduplicate values' do
+    assert_equal @list.search(1).size, 2
+    @list.deduplicate
+    assert_equal @list.search(1).size, 1
+  end
+
+  it 'should deduplicate values without an external data structure' do
+    assert_equal @list.search(29).size, 2
+    @list.deduplicate_vanilla
+    assert_equal @list.search(29).size, 1
   end
 end
