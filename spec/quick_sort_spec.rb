c0f2ff4cd41da9f58000e14f02b7e4b15973907e
Andrew Sinner <andrewsinner@gmail.com>
2014-11-03 21:11:32 -0800

    Appease the cop

--- spec/quick_sort_spec.rb
+++ spec/quick_sort_spec.rb
@@ -6,9 +6,9 @@ require 'benchmark'
 describe Array do
   it 'should sort quick sort an array' do
     array = (1..10_000).to_a.shuffle
-    assert (1..10_000).to_a, array.sorts(:quick)
+    assert((1..10_000).to_a, array.sorts(:quick))
   end
-  
+
   it 'should be benchmarked' do
     puts 'Quick sort -----------------------------'
     array = (1..1000).to_a
